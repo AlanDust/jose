d1d57cd091303b574f8157ef45d94cc3e7ad3df1
Alan Dust <adust1@gmail.com>
2019-01-10 13:55:57 -0500

    added routes and controllers

---
+++ app/controllers/api/v1/daytwo_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::DaytwoController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def index
+    render json: { artwork: Daytwo.all, current_user: current_user }
+  end
+
+  def create
+    new_dayone = Daytwo.new(daytwo_params)
+
+    if new_dayone.save
+      render json: new_daytwo
+    else
+      render json: { errors: new_daytwo.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: Daytwo.find(params[:id])
+  end
+
+  private
+
+  def daytwo_params
+    params.require(:daytwo).permit(:picture)
+  end
+end
